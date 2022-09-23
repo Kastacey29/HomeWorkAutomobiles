24c94a73937df59503813d15e3d2a820d031d9a8
Stacey29 <Kastacey@yandex.ru>
2022-09-23 19:05:52 +0300

    задание сделано

---
+++ src/Car.java
@@ -0,0 +1,53 @@
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", productionYear=" + productionYear +
+                ", productionCountry='" + productionCountry + '\'' +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+//Lada Grande, 2015 год выпуска, сборка в России, желтого цвета, объем двигателя - 1,7
+//        Audi A8 50 L TDI quattro, 2020 год выпуска, сборка в Германии, черный цвет кузова, объем двигателя 3.0 литра
+//        BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем – 3.0
+//        Kia Sportage 4 поколение, 2018 год выпуска, сборка в Южной Корее, цвет кузова красный, объем двигателя – 2,4 литра.
+//        Hyundai Avante, сборка в Южной Корее, цвет кузова оранжевый, объем двигателя - 1,6 литра, год выпуска - 2016 год.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car lada = new Car("Lada", "Grande", 1.7, "yellow", 2015, "Russia");
+        Car audi = new Car("Audi", "A8_50_L_TDI_quattro", 3.0, "black",
+                2020, "Germany");
+        Car bmw = new Car("BMW", "Z8", 3.0, "black", 2021, "Germany");
+        Car kia = new Car("Kia", "Sportage", 2.4, "red", 2018, "SouthKorea");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "orange", 2016,
+                "SouthKorea");
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
     }
 }
