5760b6ff62bae19b419ae6aa6d904931ff5f27a6
Stacey29 <Kastacey@yandex.ru>
2022-09-23 21:10:44 +0300

    задание сделано

--- src/Car.java
+++ src/Car.java
@@ -6,13 +6,39 @@ public class Car {
     int productionYear;
     String productionCountry;
 
-    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+    public Car(String brand, String model, double engineVolume, String color,
+               int productionYear, String productionCountry) {
+        if (brand==null) {
+            this.brand="default";
+        }else {
+            this.brand = brand;
+        }
+        if (model==null) {
+            this.model="default";
+        }else {
+            this.model = model;
+        }
+        if (color==null) {
+            this.color="white";
+        }else {
+            this.color = color;
+        }
+        if (productionCountry==null) {
+            this.productionCountry="default";
+        }else {
+            this.productionCountry = productionCountry;
+        }
+        if (engineVolume==0.0) {
+            this.engineVolume=1.5;
+        }else {
+            this.engineVolume = engineVolume;
+        }
+        if (productionYear==0) {
+            this.productionYear=2000;
+        }else {
+            this.productionYear = productionYear;
+        }
+
     }
 
     public String getBrand() {
